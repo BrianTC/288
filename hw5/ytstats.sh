00cb0e63b425071d99e2233144f244d9e3f4ad24
Brian <milotic68@gmail.com>
2017-02-28 23:52:48 -0500

    Made a BIG file to see if I cant figure out why its running out of memory?

--- hw5/ytstats.sh
+++ hw5/ytstats.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+FNAME=stats.csv
 views=(`cat index.html | egrep -o "[0-9][0-9]?[0-9]?(,[0-9]+)* views" | cut -d" " -f1`)
 IFS=$'\n'
 duration=(`cat index.html | egrep -o "Duration: ([0-9]+:)*[0-9]+" | cut -d" " -f2`)
@@ -8,8 +9,10 @@ vid_id=(`cat index.html | egrep -o "watch\?v=[a-zA-Z0-9_-]+"`)
 IFS=$'\n'
 title=(`cat index.html | egrep -o "dir=\"ltr\">.*<" | cut -d">" -f2 | cut -d"<" -f1`)
 #echo Title,Username,Views,Duration,Vid_ID > stats.csv
-echo "">stats.csv
-for (( i=0;i<${#views[@]};i++ )); do
-	echo ${title[$i]//,/},${unames[$i]//,/},${views[$i]//,/},${duration[$i]},${vid_id[ $((i * 2)) ]} >> stats.csv
-done
+echo "">${FNAME}
+for (( j=0; j<1 ;j++ )) do 
+	for (( i=0;i<${#views[@]};i++ )); do
+		echo ${title[$i]//,/},${unames[$i]//,/},${views[$i]//,/},${duration[$i]},${vid_id[ $((i * 2)) ]} >> ${FNAME}
+	done
 
+done
